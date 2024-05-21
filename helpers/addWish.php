c3932aba4afd35ca9cbd3198c670f3242b575f3d
remy-van-lieshout <remy@jennis.nl>
2024-05-21 14:27:21 +0200

    opzet

--- helpers/addWish.php
+++ helpers/addWish.php
@@ -2,7 +2,7 @@
 session_start();
 include '../helpers/db_connection.php';
 
-AddWish($_SESSION['user'], $_POST['beschrijving'], $_POST['url']);
+AddWish($_SESSION['user'], $_POST['beschrijving'], $_POST['url'], $_POST['max']);	
 
 header('Refresh: 0; URL =../src/Admin.php');
 ?>
